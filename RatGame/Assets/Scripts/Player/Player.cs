cbb5216929482d87cd5eb5f0fdee652967011a96
whe05 <wanming.he@tufts.edu>
2022-11-04 01:07:32 -0400

    Created a new scene to test Enemies (**with temporary art)
    Added a new scene called 'enemy':
    1. has a simple tilemap
    2. has a player that only moves around to test enemy behaviors
    3. has a chasing enemy (not finish)
    4. has a spawn point
    5. has a enemy that uses weapon to attack (not finish)
    
    **All art are downloaded online so will be replaced later

---
+++ RatGame/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class Player : Actor {
+
+      public Rigidbody2D rb;
+      public float moveSpeed = 5f;
+      public Vector2 movement;
+
+      // Auto-load the RigidBody component into the variable:
+      void Start(){
+            rb = GetComponent<Rigidbody2D> ();
+      }
+
+      // Listen for player input to move the object:
+      void FixedUpdate(){
+            movement.x = Input.GetAxisRaw ("Horizontal");
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+            movement.y = Input.GetAxisRaw ("Vertical");
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+      }
+}
