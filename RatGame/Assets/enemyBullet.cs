c69a8b98803493205a489f786a16b3477de3437e
hkomal01 <98800239+hkomal01@users.noreply.github.com>
2022-11-15 10:54:29 -0500

    shooter now shoots at you

---
+++ RatGame/Assets/enemyBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBullet : MonoBehaviour
+{
+    public GameObject player;
+    public float force;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Entity");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- RatGame/Assets/shooterOneScript.cs
+++ RatGame/Assets/shooterOneScript.cs
@@ -6,12 +6,13 @@ public class shooterOneScript : MonoBehaviour
 {
     public GameObject bullet;
     public Transform bulletPos;
+    public float bulletLife = 5.0f;
 
     private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bullet = Instantiate(bullet, bulletPos.position, Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -20,13 +21,14 @@ public class shooterOneScript : MonoBehaviour
         timer += Time.deltaTime;
 
         if(timer > 2){
-            timer = 0;
+            timer = timer - 2;
             shoot();
         }
     }
 
     void shoot()
     {
-        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        GameObject clone = Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        Destroy(clone, bulletLife);
     }
 }
