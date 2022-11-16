d2626fdb4e7f381496a44a249120d81c17b70978
whe05 <wanming.he@tufts.edu>
2022-11-15 22:25:03 -0500

    Get rid some animations and temp art

--- RatGame/Assets/player1.cs
+++ RatGame/Assets/player1.cs
@@ -2,37 +2,24 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public class player1 : Actor {
+public class player1 : MonoBehaviour {
 
       public Rigidbody2D rb;
       public float moveSpeed = 5f;
       public Vector2 movement;
-      
-      private float moveSpeedMultiplier;
-      private float moveHorizontal;
-      private float moveVertical;
+
       // Auto-load the RigidBody component into the variable:
-      void Start(){
+       void Start(){
             rb = GetComponent<Rigidbody2D> ();
-            moveSpeedMultiplier = 0.3f;
-
-      }
-
-      void Update() {
-            moveHorizontal = Input.GetAxisRaw("Horizontal");
-            moveVertical = Input.GetAxisRaw("Vertical");
+            rb.freezeRotation = true;
       }
 
       // Listen for player input to move the object:
       void FixedUpdate(){
-            // movement.x = Input.GetAxisRaw ("Horizontal");
-            // rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-            rb.AddForce(new Vector2(moveHorizontal*moveSpeedMultiplier, moveVertical*moveSpeedMultiplier),ForceMode2D.Impulse);
-
-            // movement.y = Input.GetAxisRaw ("Vertical");
-            // movement.x = Input.GetAxisRaw("Horizontal");
-            // movement = movement.normalized;
-            // rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            movement = movement.normalized;
+            rb.MovePosition(rb.position + movement * moveSpeed);
       
       }
 }
